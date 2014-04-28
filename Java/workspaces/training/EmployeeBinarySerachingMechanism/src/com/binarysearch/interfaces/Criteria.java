49ff37e4a3fb8ea53c5ce3be9565e40cd4ce8984
Praveen Ravipati <ravipatipraveen77.45@gmail.com>
2014-04-28 18:36:37 +0530

    removed

--- Java/workspaces/training/EmployeeBinarySerachingMechanism/src/com/binarysearch/interfaces/Criteria.java
+++
@@ -1,5 +0,0 @@
-package com.binarysearch.interfaces;
-
-public interface Criteria {
-	public int checkCodition(Object Element, Object searchKey);
-}
--- Java/workspaces/training/EmployeeRecordsFilter/src/com/vlabs/employee/conditions/Condition.java
+++
@@ -1,5 +0,0 @@
-package com.vlabs.employee.conditions;
-
-public interface Condition {
-	public Object condition(Object employeeRecord,Object detailToSearch);
-}
